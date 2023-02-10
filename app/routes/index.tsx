bcaeff95e274a090e068bcc074e990559d02efd5
Gry Nagel <grynag@gmail.comm>
2023-02-10 18:03:19 +0100

    Move graph to landingpage

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,18 +1,9 @@
-import { Link, useLoaderData } from '@remix-run/react';
-import { addHours, isBefore, set, setMinutes } from 'date-fns';
+import { isBefore, set } from 'date-fns';
 import type { LinksFunction } from '@remix-run/react/dist/routeModules';
 import type { HeadersFunction, LoaderFunction } from '@remix-run/node';
-
-import { createViewTime, getHours } from '../utils/date';
-import Footer from '../components/Footer';
-import Header from '../components/Header';
+import { redirect } from '@remix-run/node';
 
 import indexStylesUrl from '~/styles/index.css';
-import type { AveragePrices } from '~/types';
-import { areas, getEntries, getKeys } from '~/types';
-import { getTodaysPrices } from '~/server/api.server';
-import { getAveragePrice } from '~/server/average.server';
-import AllPricesChart from '~/components/AllPricesChart';
 
 export const links: LinksFunction = () => {
   return [{ rel: 'stylesheet', href: indexStylesUrl }];
@@ -35,60 +26,6 @@ export const headers: HeadersFunction = () => {
   };
 };
 
-type LoaderData = {
-  prices: Awaited<ReturnType<typeof getTodaysPrices>>;
-  averagePrices: AveragePrices;
-};
-
 export const loader: LoaderFunction = async () => {
-  const prices = await getTodaysPrices();
-
-  const averagePrices = getEntries(prices.priceByHour.pricesObj).reduce((acc, [key, value]) => {
-    acc[key] = getAveragePrice(value);
-    return acc;
-  }, {} as AveragePrices);
-
-  return { prices, averagePrices };
+  return redirect('/area');
 };
-
-export default function IndexRoute() {
-  const { prices, averagePrices } = useLoaderData() as LoaderData;
-
-  return (
-    <div>
-      <Header />
-      <div className="container">
-        <h2 className="price-header">
-          Strømprisen her og nå i øre/kWh ({createViewTime(setMinutes(new Date(), 0))} -{' '}
-          {createViewTime(addHours(setMinutes(new Date(), 0), 1))}):
-        </h2>
-        <nav>
-          <ul className="navigation-list">
-            {getKeys(prices.price).map((key) => (
-              <Link
-                to={`/price/${areas[key].number}`}
-                key={key}
-                className={`navigation-link link-${areas[key].number}`}
-                style={{
-                  borderBottom: `20px solid var(--chart-${areas[key].number})`,
-                  borderTop: `20px solid var(--chart-${areas[key].number})`,
-                }}
-              >
-                <li className="navigation-item">
-                  <h2 className="header">{areas[key].title}</h2>
-                  <p className="price">{prices.priceByHour.pricesObj[key][getHours()]}</p>
-                  <p>
-                    Dagens gjennomsnitt: <br />
-                    <strong>{averagePrices[key]}</strong>
-                  </p>
-                </li>
-              </Link>
-            ))}
-          </ul>
-        </nav>
-        <AllPricesChart data={averagePrices} />
-      </div>
-      <Footer />
-    </div>
-  );
-}
